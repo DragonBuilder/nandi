32a8e5c33479c8afe5fdb4c1e5eead3696d06773
Aneesh Anil <aneesh@geektrust.in>
2024-07-14 20:22:32 +0530

    removed unnecessary code

--- main.go
+++ main.go
@@ -27,32 +27,9 @@ func main() {
 	http.ListenAndServe(fmt.Sprintf(":%s", port), r)
 }
 
-type User struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
 type ErrorResponse struct {
 	Error string `json:"error"`
 }
-
-// func (e ErrorResponse) Render(w http.ResponseWriter, _ *http.Request) error {
-// 	w.WriteHeader(e.httpStatus)
-// 	return nil
-// }
-
-func ping(w http.ResponseWriter, r *http.Request) {
-}
-
-// func SignUp(w http.ResponseWriter, r *http.Request) {
-// 	u := UserSignUp{}
-// 	if err := render.Decode(r, &u); err != nil {
-// 		slog.Error(fmt.Sprintf("ErrMashallingSignUpUser : %v", err))
-// 		render.Render(w, r, ErrorResponse{"sign-up failed", http.StatusInternalServerError})
-// 	}
-
-// }
 
 func EnvVar(name string) string {
 	return os.Getenv(name)
